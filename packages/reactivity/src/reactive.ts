4418aa3adc9c14fd40a5af0483e380df80e96a0f
fan <291950304@qq.com>
2024-04-30 15:48:48 +0800

    feat: reactive

---
+++ packages/reactivity/src/reactive.ts
@@ -0,0 +1,28 @@
+import { isObject } from '@vue/shared'
+import { mutableHandlers } from './baseHandlers'
+export enum ReactiveFlags {
+    IS_REACTIVE = '__v_isReactive',
+}
+
+const reactiveMap = new WeakMap() // key 只能是对象
+
+export function reactive(target: object) {
+    if (!isObject(target)) {
+        // 不对非对象类型进行处理
+        return target
+    }
+    // 如果取值的时候走了代理对象的 get 方法，便是被代理过的，并没有增加这个属性
+    if (target[ReactiveFlags.IS_REACTIVE]) {
+        return target
+    }
+    // 对象被代理过直接 return
+    const existProxy = reactiveMap.get(target)
+    if (existProxy) {
+        return existProxy
+    }
+    // 对对象进行代理
+    const proxy = new Proxy(target, mutableHandlers)
+    // 缓存一下，代理过的对象，下次再进行代理的时候，直接拿出来用即可
+    reactiveMap.set(target, proxy)
+    return proxy
+}
