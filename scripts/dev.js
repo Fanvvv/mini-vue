137cfd3990ded0f372574df0c45e53365a6f137f
fan <291950304@qq.com>
2024-05-06 15:06:20 +0800

    fix: import dirname and fileURLToPath

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,12 +1,14 @@
 // 使用 esbuild 加快开发构建速度。
 // 仍使用 Rollup 进行生产构建，因为它会生成更小的文件，并提供更好的 tree-shaking 效果。
 
-import {resolve} from "node:path";
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { createRequire } from 'node:module'
-import {context} from "esbuild";
-import minimist from "minimist";
+import { context } from 'esbuild'
+import minimist from 'minimist'
 
 const require = createRequire(import.meta.url)
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // 解析用户执行命令行的参数
 const args = minimist(process.argv.slice(2))
@@ -17,9 +19,16 @@ const target = args._[0] || 'reactivity'
 // 读取对应的 package.json 内容
 const pkg = require(resolve(__dirname, `../packages/${target}/package.json`))
 // 打包输出的格式， 如果为 global 则使用 iife 自执行函数的方式
-const outputFormat = format.startsWith('global') ? 'iife' : format === 'cjs' ? 'cjs' : 'esm'
+const outputFormat = format.startsWith('global')
+    ? 'iife'
+    : format === 'cjs'
+      ? 'cjs'
+      : 'esm'
 // 输出文件
-const outfile = resolve(__dirname, `../packages/${target}/dist/${target}.${format}.js`)
+const outfile = resolve(
+    __dirname,
+    `../packages/${target}/dist/${target}.${format}.js`,
+)
 
 // esbuild 打包
 context({
@@ -29,8 +38,8 @@ context({
     sourcemap: true,
     format: outputFormat,
     globalName: pkg.buildOptions?.name,
-    platform: format === "cjs" ? "node" : "browser",
+    platform: format === 'cjs' ? 'node' : 'browser',
 }).then(ctx => {
     ctx.watch()
-    console.log("watching~~~~")
+    console.log('watching~~~~')
 })
