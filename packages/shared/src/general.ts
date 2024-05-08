33abb9845fc6f1f46f35eab08f9012eec1393fd8
fan <291950304@qq.com>
2024-05-08 10:46:14 +0800

    feat: add isPlainObject method

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -6,3 +6,10 @@ export const isObject = (val: unknown): val is Record<any, any> =>
 // 判断类型是否为函数
 export const isFunction = (val: unknown): val is Function =>
     typeof val === 'function'
+// toString 方法
+export const objectToString = Object.prototype.toString
+export const toTypeString = (value: unknown): string =>
+    objectToString.call(value)
+// 判断是否为原始对象
+export const isPlainObject = (val: unknown): val is object =>
+    toTypeString(val) === '[object Object]'
