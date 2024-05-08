e4c5ea9640c9bf80e6571a45d31945a23bce5aa2
fan <291950304@qq.com>
2024-05-08 17:57:33 +0800

    chore: add hasChanged method

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -1,15 +1,23 @@
 // 空操作
 export const NOOP = () => {}
+
 // 判断类型是否为对象
 export const isObject = (val: unknown): val is Record<any, any> =>
     val !== null && typeof val === 'object'
+
 // 判断类型是否为函数
 export const isFunction = (val: unknown): val is Function =>
     typeof val === 'function'
+
 // toString 方法
 export const objectToString = Object.prototype.toString
 export const toTypeString = (value: unknown): string =>
     objectToString.call(value)
+
 // 判断是否为原始对象
 export const isPlainObject = (val: unknown): val is object =>
     toTypeString(val) === '[object Object]'
+
+// 比较值是否有更改，并考虑NaN
+export const hasChanged = (value: any, oldValue: any): boolean =>
+    !Object.is(value, oldValue)
