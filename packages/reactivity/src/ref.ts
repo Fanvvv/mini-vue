cb847a08a55e441a0c2bd861d7fdb9326c44c5c2
fan <291950304@qq.com>
2024-05-09 18:12:34 +0800

    feat: added toRef method

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,4 @@
-import { hasChanged, isObject } from '@vue/shared'
+import { hasChanged, isFunction, isObject } from '@vue/shared'
 import { reactive } from './reactive'
 import { activeEffect, trackEffects, triggerEffects } from './effect'
 import type { ComputedRef } from './computed'
@@ -68,4 +68,56 @@ class RefImpl<T> {
  */
 export function unref<T>(ref: MaybeRef<T> | ComputedRef<T>): T {
     return isRef(ref) ? ref.value : ref
+}
+
+class ObjectRefImpl<T extends object, K extends keyof T> {
+    public __v_isRef = true
+
+    constructor(
+        private readonly object: T,
+        private readonly key: K,
+    ) {}
+
+    get value() {
+        return this.object[this.key]
+    }
+
+    set value(newVal) {
+        this.object[this.key] = newVal
+    }
+}
+
+function propertyToRef(source: Record<string, any>, key: string) {
+    const val = source[key]
+    return isRef(val) ? val : (new ObjectRefImpl(source, key) as any)
+}
+
+class GetterRefImpl<T> {
+    public readonly __v_isRef = true
+    public readonly __v_isReadonly = true
+
+    constructor(private readonly _getter: () => T) {}
+
+    get value() {
+        return this._getter()
+    }
+}
+
+export function toRef(source, key?: string): Ref {
+    if (isRef(source)) {
+        // 如果是 ref 则直接返回
+        return source
+    } else if (isFunction(source)) {
+        // return source()
+        // 直接调用source()是对函数的一次性调用，得到一个立即值。
+        // 而new GetterRefImpl(source)创建了一个可以被Vue追踪并在其响应式系统中作为只读引用使用的对象，它延迟了source函数的执行，直到实际需要这个值的时候
+        // .value 的时候再取值
+        return new GetterRefImpl(source) as any
+    } else if (isObject(source) && arguments.length > 1) {
+        // 假如传入的是对象，必须传入 key，也需要转成 ref
+        return propertyToRef(source, key!)
+    } else {
+        // 返回个 ref
+        return ref(source)
+    }
 }
