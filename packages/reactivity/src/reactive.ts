1a627be1359c1d4ae12e5e12881e5a90c4d91ceb
fan <291950304@qq.com>
2024-10-30 09:15:52 +0800

    chore: 添加注释

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -17,10 +17,11 @@ export function reactive(target) {
         return target
     }
     // 如果取值的时候走了代理对象的 get 方法，便是被代理过的，并没有增加这个属性
+    // 可以处理嵌套的响应式对象和 readonly 对象
     if (target[ReactiveFlags.IS_REACTIVE]) {
         return target
     }
-    // 对象被代理过直接 return
+    // 对象被代理过直接 return，确保对同一个原始对象不会创建多个代理
     const existProxy = reactiveMap.get(target)
     if (existProxy) {
         return existProxy
