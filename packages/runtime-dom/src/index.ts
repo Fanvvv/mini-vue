5d1c10860fa806fc75d9c558379d4c38a2359bc4
fan <291950304@qq.com>
2024-11-20 14:28:27 +0800

    chore: 导出使用默认配置的render方法

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,3 +1,15 @@
-export * from './nodeOps'
+import { nodeOps } from './nodeOps'
+import { patchProp } from './patchProp'
+import { createRenderer } from './renderer'
+
+const renderOptions = Object.assign(nodeOps, { patchProp })
+
+/**
+ * 用户可以使用 createRender 自定义 render 方法
+ * 也可以使用提供默认配置的 render 方法
+ */
+export const render = (vnode, container) => {
+    return createRenderer(renderOptions).render(vnode, container)
+}
 
 export * from '@vue/runtime-core'
