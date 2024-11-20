4f05065fad788675295b119f0c2e9aa108c92240
fan <291950304@qq.com>
2024-11-20 14:27:19 +0800

    chore: isSameVNodeType function

--- packages/runtime-core/src/vnode.ts
+++ packages/runtime-core/src/vnode.ts
@@ -5,6 +5,11 @@ export function isVNode(value: any) {
     return value ? value.__v_isVNode === true : false
 }
 
+// 判断是否为同一个节点类型
+export function isSameVNodeType(n1, n2) {
+    return n1.type === n2.type && n1.key === n2.key
+}
+
 export function createVNode(type, props = null, children = null) {
     // 元素的类型有：文本、元素、组件、keep-alive......
     // 用标识来区分对应的虚拟节点类型
